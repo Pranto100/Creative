cb224682206ef867c025c8da4d3ba3761b996894
saragitiub <115100157+saragitiub@users.noreply.github.com>
2024-04-15 06:51:41 +0600

    Inventory Manager Dashboard Scene + controller

---
+++ IMDashboardSceneController.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Mainpkg;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class IMDashboardSceneController implements Initializable {
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void shopButtonOnClick(ActionEvent event) throws IOException {
+        Parent anchorpane = FXMLLoader.load(getClass().getResource("IMBuyingScene.fxml"));
+        
+       Scene shopScene = new Scene(anchorpane);
+       
+       Stage stgToShop = (Stage)((Node) event.getSource()).getScene().getWindow();
+       stgToShop.setScene(shopScene);
+       stgToShop.show();
+    }
+
+    @FXML
+    private void AddInventoryButtonOnClick(ActionEvent event) throws IOException {
+       Parent anchorpane = FXMLLoader.load(getClass().getResource("IMWarehouseScene.fxml"));
+        
+       Scene shopScene = new Scene(anchorpane);
+       
+       Stage stgToWarehouse = (Stage)((Node) event.getSource()).getScene().getWindow();
+       stgToWarehouse.setScene(shopScene);
+       stgToWarehouse.show();
+    }
+    
+    @FXML
+    private void DataStatButtonOnClick(ActionEvent event) {
+    }
+    }
+
+
+    
+    
+
